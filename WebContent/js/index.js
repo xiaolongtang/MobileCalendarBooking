8bb9919124b055c7579940d947684394e7bb50ba
Tang <xiaolong.tang@pactera.com>
2016-08-18 13:54:38 +0800

    calendard is on

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,280 +1,124 @@
-angular.module('starter', ['ionic'])
+angular.module('starter', [ 'ionic', 'ngAnimate', 'ui.rCalendar' ])
 
-        .run(function ($ionicPlatform) {
-            $ionicPlatform.ready(function () {
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
-                if (window.cordova && window.cordova.plugins.Keyboard) {
-                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                }
-                if (window.StatusBar) {
-                    // org.apache.cordova.statusbar required
-                    StatusBar.styleDefault();
-                }
-            });
-        })
+.run(function($ionicPlatform, $animate) {
+	'use strict';
+	$animate.enabled(false);
+})
 
-        .config(function ($stateProvider, $urlRouterProvider) {
-            $stateProvider
+.config(function($stateProvider, $urlRouterProvider) {
+	'use strict';
+	$stateProvider.state('tabs', {
+		url : '/tab',
+		abstract : true,
+		templateUrl : 'templates/tabs.html'
+	}).state('tabs.home', {
+		url : '/home',
+		views : {
+			'home-tab' : {
+				templateUrl : 'templates/home.html',
+				controller : 'CalendarDemoCtrl'
+			}
+		}
+	}).state('tabs.about', {
+		url : '/about',
+		views : {
+			'about-tab' : {
+				templateUrl : 'templates/about.html'
+			}
+		}
+	}).state('tabs.contact', {
+		url : '/contact',
+		views : {
+			'contact-tab' : {
+				templateUrl : 'templates/contact.html'
+			}
+		}
+	});
 
-                    .state('app', {
-                        url: "/app",
-                        abstract: true,
-                        templateUrl: "menu.html",
-                        controller: 'AppCtrl'
-                    })
+	$urlRouterProvider.otherwise('/tab/home');
+})
 
-                    .state('app.playlists', {
-                        url: "/playlists",
-                        views: {
-                            'menuContent': {
-                                templateUrl: "playlists.html",
-                                controller: 'PlaylistsCtrl'
-                            }
-                        }
-                    })
-                    
-                    .state('app.Beijing', {
-                        url: "/Beijing",
-                        views: {
-                            'menuContent': {
-                                templateUrl: "Beijing.html",
-                                controller: 'BeijingCtrl'
-                            }
-                        }
-                    })
-                    
-                    .state('app.Shenzhen', {
-                        url: "/Shenzhen",
-                        views: {
-                            'menuContent': {
-                                templateUrl: "Shenzhen.html",
-                                controller: 'ShenzhenCtrl'
-                            }
-                        }
-                    })
-                    
-                     .state('app.Shanghai', {
-                        url: "/Shanghai",
-                        views: {
-                            'menuContent': {
-                                templateUrl: "Shanghai.html",
-                                controller: 'ShanghaiCtrl'
-                            }
-                        }
-                    })
+.controller('CalendarDemoCtrl',function($scope) {
+			'use strict';
+			$scope.calendar = {};
+			$scope.changeMode = function(mode) {
+				$scope.calendar.mode = mode;
+			};
 
+			$scope.loadEvents = function() {
+				$scope.calendar.eventSource = createRandomEvents();
+			};
 
-            // if none of the above states are matched, use this as the fallback
-            $urlRouterProvider.otherwise('/app/playlists');
-        })
+			$scope.onEventSelected = function(event) {
+				console.log('Event selected:' + event.startTime + '-'
+						+ event.endTime + ',' + event.title);
+			};
 
-        .controller('AppCtrl', function ($scope,$http) {
-        	$scope.locations=[];
-        	$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=temperature&location=dl').success(function(data){
-        		$scope.locations.push({location:'Dalian',temp:data.value});   		
-    		});
-        	
-        	$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=temperature&location=bj').success(function(data){
-        		$scope.locations.push({location:'Beijing',temp:data.value});
-        	});
-        	
-            $http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=temperature&location=sz').success(function(data){
-            	$scope.locations.push({location:'Shenzhen',temp:data.value});
-        	});
-            
-            $http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=temperature&location=sh').success(function(data){
-            	$scope.locations.push({location:'Shanghai',temp:data.value});
-        	});
-        })
+			$scope.onViewTitleChanged = function(title) {
+				$scope.viewTitle = title;
+			};
 
-        .controller('PlaylistsCtrl', function ($scope,$http) {
-//            $scope
-            console.log("controller running...");
-            var updateStatus = function(){
-                console.log("function running...");
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=pm2.5&location=dl').success(function(data){
-	    			$scope.pm25=data;
-//                                console.log(data);
-	    			$scope.pm25c=getColor(data.value);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=pm10&location=dl').success(function(data){
-	    			$scope.pm10=data;
-//                                console.log(data);
-                    $scope.pm10c=getColor(data.value);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=pm1.0&location=dl').success(function(data){
-	    			$scope.pm1=data;
-	    			$scope.pm1c=getColor(data.value);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=temperature&location=dl').success(function(data){
-	    			$scope.temperature=data;
-                                console.log(data);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=humidity&location=dl').success(function(data){
-	    			$scope.humidity=data;
-                                console.log(data);
-	    		});
-	    	};
-    	    setInterval(function(){
-            $scope.$apply(updateStatus);
-             },10000);
-             
-             updateStatus();
-             
-             var getColor=function (num){
-            	 if(num<=50 &&num>0){
-            		 return "#3ebf6c";
-            	 }else if(num>50 && num<=100){
-            		 return "#d7af0c";
-            	 }else if(num>100 && num<=150){
-            		 return "#dd0000";
-            	 }else{
-            		 return "#6d6d6d";
-            	 }
-             };
-             
-//            $scope.apply(updateStatus);
-//            $scope.apply(setInterval);
-        })
-        
-          .controller('BeijingCtrl', function ($scope,$http) {
-            console.log("controller running...");
-            var updateStatus = function(){
-                console.log("function running...");
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=pm2.5&location=bj').success(function(data){
-	    			$scope.pm25=data;
-//                                console.log(data);
-	    			$scope.pm25c=getColor(data.value);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=pm10&location=bj').success(function(data){
-	    			$scope.pm10=data;
-//                                console.log(data);
-                    $scope.pm10c=getColor(data.value);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=pm1.0&location=bj').success(function(data){
-	    			$scope.pm1=data;
-	    			$scope.pm1c=getColor(data.value);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=temperature&location=bj').success(function(data){
-	    			$scope.temperature=data;
-                                console.log(data);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=humidity&location=bj').success(function(data){
-	    			$scope.humidity=data;
-                                console.log(data);
-	    		});
-	    	};
-    	    setInterval(function(){
-            $scope.$apply(updateStatus);
-             },10000);
-             
-             updateStatus();
-             
-             var getColor=function (num){
-            	 if(num<=50 &&num>0){
-            		 return "#3ebf6c";
-            	 }else if(num>50 && num<=100){
-            		 return "#d7af0c";
-            	 }else if(num>100 && num<=150){
-            		 return "#dd0000";
-            	 }else{
-            		 return "#6d6d6d";
-            	 }
-             };
-             
-        })
-        
-           .controller('ShenzhenCtrl', function ($scope,$http) {
-            console.log("controller running...");
-            var updateStatus = function(){
-                console.log("function running...");
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=pm2.5&location=sz').success(function(data){
-	    			$scope.pm25=data;
-//                                console.log(data);
-	    			$scope.pm25c=getColor(data.value);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=pm10&location=sz').success(function(data){
-	    			$scope.pm10=data;
-//                                console.log(data);
-                    $scope.pm10c=getColor(data.value);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=pm1.0&location=sz').success(function(data){
-	    			$scope.pm1=data;
-	    			$scope.pm1c=getColor(data.value);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=temperature&location=sz').success(function(data){
-	    			$scope.temperature=data;
-                                console.log(data);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=humidity&location=sz').success(function(data){
-	    			$scope.humidity=data;
-                                console.log(data);
-	    		});
-	    	};
-    	    setInterval(function(){
-            $scope.$apply(updateStatus);
-             },10000);
-             
-             updateStatus();
-             
-             var getColor=function (num){
-            	 if(num<=50 &&num>0){
-            		 return "#3ebf6c";
-            	 }else if(num>50 && num<=100){
-            		 return "#d7af0c";
-            	 }else if(num>100 && num<=150){
-            		 return "#dd0000";
-            	 }else{
-            		 return "#6d6d6d";
-            	 }
-             };
-             
-        })
-       
-          .controller('ShanghaiCtrl', function ($scope,$http) {
-            console.log("controller running...");
-            var updateStatus = function(){
-                console.log("function running...");
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=pm2.5&location=sh').success(function(data){
-	    			$scope.pm25=data;
-//                                console.log(data);
-	    			$scope.pm25c=getColor(data.value);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=pm10&location=sh').success(function(data){
-	    			$scope.pm10=data;
-//                                console.log(data);
-                    $scope.pm10c=getColor(data.value);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=pm1.0&location=sh').success(function(data){
-	    			$scope.pm1=data;
-	    			$scope.pm1c=getColor(data.value);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=temperature&location=sh').success(function(data){
-	    			$scope.temperature=data;
-                                console.log(data);
-	    		});
-	    		$http.get('https://smart-web-wechat.run.aws-usw02-pr.ice.predix.io/value?type=humidity&location=sh').success(function(data){
-	    			$scope.humidity=data;
-                                console.log(data);
-	    		});
-	    	};
-    	    setInterval(function(){
-            $scope.$apply(updateStatus);
-             },10000);
-             
-             updateStatus();
-             
-             var getColor=function (num){
-            	 if(num<=50 &&num>0){
-            		 return "#3ebf6c";
-            	 }else if(num>50 && num<=100){
-            		 return "#d7af0c";
-            	 }else if(num>100 && num<=150){
-            		 return "#dd0000";
-            	 }else{
-            		 return "#6d6d6d";
-            	 }
-             };
-             
-        })
+			$scope.today = function() {
+				$scope.calendar.currentDate = new Date();
+			};
+
+			$scope.isToday = function() {
+				var today = new Date(), currentCalendarDate = new Date(
+						$scope.calendar.currentDate);
+
+				today.setHours(0, 0, 0, 0);
+				currentCalendarDate.setHours(0, 0, 0, 0);
+				return today.getTime() === currentCalendarDate.getTime();
+			};
+
+			$scope.onTimeSelected = function(selectedTime, events) {
+				console.log('Selected time: ' + selectedTime + ', hasEvents: '
+						+ (events !== undefined && events.length !== 0));
+			};
+
+			function createRandomEvents() {
+				var events = [];
+				for (var i = 0; i < 50; i += 1) {
+					var date = new Date();
+					var eventType = Math.floor(Math.random() * 2);
+					var startDay = Math.floor(Math.random() * 90) - 45;
+					var endDay = Math.floor(Math.random() * 2) + startDay;
+					var startTime;
+					var endTime;
+					if (eventType === 0) {
+						startTime = new Date(Date.UTC(date.getUTCFullYear(),
+								date.getUTCMonth(), date.getUTCDate()
+										+ startDay));
+						if (endDay === startDay) {
+							endDay += 1;
+						}
+						endTime = new Date(Date.UTC(date.getUTCFullYear(), date
+								.getUTCMonth(), date.getUTCDate() + endDay));
+						events.push({
+							title : 'All Day - ' + i,
+							startTime : startTime,
+							endTime : endTime,
+							allDay : true
+						});
+					} else {
+						var startMinute = Math.floor(Math.random() * 24 * 60);
+						var endMinute = Math.floor(Math.random() * 180)
+								+ startMinute;
+						startTime = new Date(date.getFullYear(), date
+								.getMonth(), date.getDate() + startDay, 0, date
+								.getMinutes()
+								+ startMinute);
+						endTime = new Date(date.getFullYear(), date.getMonth(),
+								date.getDate() + endDay, 0, date.getMinutes()
+										+ endMinute);
+						events.push({
+							title : 'Event - ' + i,
+							startTime : startTime,
+							endTime : endTime,
+							allDay : false
+						});
+					}
+				}
+				return events;
+			}
+		});
